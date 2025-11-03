053385afdaadf73deb001efda1b7857de6a8dee4
yzj <you@example.comyeahzz929@gmail.com>
2025-11-02 22:27:12 -0600

    修复根路径路由，添加 API 欢迎页面

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@
  * Connect all of your endpoints together here.
  */
 module.exports = function (app, router) {
+    // Root path - API information
+    app.get('/', function (req, res) {
+        res.json({
+            message: 'Welcome to CS409 MP3 API',
+            data: {
+                endpoints: {
+                    users: '/api/users',
+                    tasks: '/api/tasks',
+                    home: '/api/'
+                },
+                documentation: 'This is a REST API for managing users and tasks'
+            }
+        });
+    });
+
     app.use('/api', require('./home.js')(router));
     app.use('/api', require('./users.js')(router));
     app.use('/api', require('./tasks.js')(router));
